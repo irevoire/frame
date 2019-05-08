88c979667bd32c765128df11157245b31811ced6
Thomas Campistron <irevoire@hotmail.fr>
2019-05-08 13:19:03 +0200

    propose an unsafe function to get the representation of a struct in memory and code the serialize function

--- src/field/ether.rs
+++ src/field/ether.rs
@@ -1,16 +1,19 @@
-#[macro_use]
 use derive_builder::Builder;
 
+#[repr(packed)]
 #[derive(Builder)]
 struct Ether {
+    #[builder(default = "[0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]")] // broadcast
     dst_addr: [u8; 6],
     src_addr: [u8; 6],
+    #[builder(default = "0x0800")] // ipv4
     ether_type: u16,
 }
 
 impl crate::field::Field for Ether {
     fn serialize(&self) -> &[u8] {
-        &self.dst_addr
+        // should be safe since everything is aligned
+        unsafe { crate::field::any_as_u8_slice(self) }
     }
 }
 
@@ -21,13 +24,33 @@ mod tests {
     #[test]
     fn builder() {
         let ether = crate::field::ether::EtherBuilder::default()
-            .src_addr([0x00, 0x11, 0x22, 0x33, 0x44, 0x55])
             .dst_addr([0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff])
-            .ether_type(42)
+            .src_addr([0x00, 0x11, 0x22, 0x33, 0x44, 0x55])
+            .ether_type(0x42)
             .build()
             .unwrap();
-        assert_eq!(ether.src_addr, [0x00, 0x11, 0x22, 0x33, 0x44, 0x55]);
-        assert_eq!(ether.dst_addr, [0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff]);
-        assert_eq!(ether.ether_type, 42);
+        unsafe {
+            assert_eq!(ether.src_addr, [0x00, 0x11, 0x22, 0x33, 0x44, 0x55]);
+            assert_eq!(ether.dst_addr, [0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff]);
+            assert_eq!(ether.ether_type, 0x42);
+        }
+    }
+
+    #[test]
+    fn serialize() {
+        let ether = crate::field::ether::EtherBuilder::default()
+            .dst_addr([0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff])
+            .src_addr([0x00, 0x11, 0x22, 0x33, 0x44, 0x55])
+            .ether_type(0x42)
+            .build()
+            .unwrap();
+        assert_eq!(
+            ether.serialize(),
+            [
+                0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff, // dst_addr
+                0x00, 0x11, 0x22, 0x33, 0x44, 0x55, //src_addr
+                0x42, 0x00, // ether_type: take care of the endianness
+            ]
+        );
     }
 }
