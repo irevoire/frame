e9067155e9b26be86c1b3b06ff67f2c8fb95ec76
Thomas Campistron <irevoire@hotmail.fr>
2019-05-08 12:58:01 +0200

    use the crate derive_builder to help defining new fields

--- src/field/ether.rs
+++ src/field/ether.rs
@@ -1,68 +1,16 @@
+#[macro_use]
+use derive_builder::Builder;
+
+#[derive(Builder)]
 struct Ether {
     dst_addr: [u8; 6],
     src_addr: [u8; 6],
     ether_type: u16,
 }
 
-impl Ether {
-    pub fn new() -> Self {
-        Ether {
-            dst_addr: [0; 6],
-            src_addr: [0; 6],
-            ether_type: 0,
-        }
-    }
-
-    pub fn src_addr(&mut self, addr: [u8; 6]) -> &mut Self {
-        self.src_addr = addr;
-        self
-    }
-
-    pub fn dst_addr(&mut self, addr: [u8; 6]) -> &mut Self {
-        self.dst_addr = addr;
-        self
-    }
-
-    pub fn ether_type(&mut self, ether_type: u16) -> &mut Self {
-        self.ether_type = ether_type;
-        self
-    }
-}
-
 impl crate::field::Field for Ether {
     fn serialize(&self) -> &[u8] {
         &self.dst_addr
-    }
-}
-
-struct EtherBuilder {
-    ether: Ether,
-}
-
-impl EtherBuilder {
-    pub fn new() -> Self {
-        EtherBuilder {
-            ether: Ether::new(),
-        }
-    }
-
-    pub fn src_addr(mut self, addr: [u8; 6]) -> Self {
-        self.ether.src_addr(addr);
-        self
-    }
-
-    pub fn dst_addr(mut self, addr: [u8; 6]) -> Self {
-        self.ether.dst_addr(addr);
-        self
-    }
-
-    pub fn ether_type(mut self, ether_type: u16) -> Self {
-        self.ether.ether_type(ether_type);
-        self
-    }
-
-    pub fn build(self) -> Ether {
-        self.ether
     }
 }
 
@@ -72,11 +20,12 @@ mod tests {
 
     #[test]
     fn builder() {
-        let ether = crate::field::ether::EtherBuilder::new()
+        let ether = crate::field::ether::EtherBuilder::default()
             .src_addr([0x00, 0x11, 0x22, 0x33, 0x44, 0x55])
             .dst_addr([0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff])
             .ether_type(42)
-            .build();
+            .build()
+            .unwrap();
         assert_eq!(ether.src_addr, [0x00, 0x11, 0x22, 0x33, 0x44, 0x55]);
         assert_eq!(ether.dst_addr, [0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff]);
         assert_eq!(ether.ether_type, 42);
